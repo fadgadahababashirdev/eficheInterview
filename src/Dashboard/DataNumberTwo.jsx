43922503b22538b75a9a7212694d6a56e9ad006a
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-10-24 09:37:48 +0200

    commit

--- src/Dashboard/DataNumberTwo.jsx
+++ src/Dashboard/DataNumberTwo.jsx
@@ -1,61 +1,75 @@
-import { Briefcase, Check, LucideClipboardList } from 'lucide-react'
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import { GoPeople } from 'react-icons/go'
-import { IoMdCodeWorking } from 'react-icons/io'
-import { IoBagHandleOutline, IoHappyOutline } from 'react-icons/io5'
+import { Briefcase, Check, LucideClipboardList } from 'lucide-react';
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { GoPeople } from 'react-icons/go';
+import { IoMdCodeWorking } from 'react-icons/io';
+import { IoBagHandleOutline, IoHappyOutline } from 'react-icons/io5';
 
 const DataNumberTwo = () => {
   return (
-    <div className=' w-full bg-white rounded-md my-[2rem] p-4 pb-[2rem]'> 
-    
-    <h1 className="para text-[1.2rem] font-medium">Inpatient</h1> 
-    <div className='py-[.6rem]'></div>
-     <div className='flex justify-between mt-[1rem] '>
-      <div className='w-full h-full border-r-[1.6px]  border-[#F1F2F4]'>
-        <div className='flex gap-4 items-center'>
-        <div className="bg-[#F8F8F8] p-2 w-[3rem] h-[3rem] flex items-center justify-center rounded-full">
+    <div className=" w-full bg-white rounded-md my-[2rem] p-4 pb-[2rem]">
+      <h1 className="para text-[1.2rem] font-medium">Inpatient</h1>
+      <div className="py-[.6rem]"></div>
+      <div className="flex justify-between mt-[1rem] ">
+        <div className="w-full h-full border-r-[1.6px]  border-[#F1F2F4]">
+          <div className="flex gap-4 items-center">
+            <div className="bg-[#F8F8F8] p-2 w-[3rem] h-[3rem] flex items-center justify-center rounded-full">
               <Check className="w-3" />
             </div>
-         <h1 className="para text-[14px] font-medium">Available <br/>beds</h1>
-           
-         </div> 
-         <div className="text-[2rem] para font-semibold mt-[1.2rem]  mb-[-.8rem]">32</div>
-      </div>
-      <div className='w-full h-full border-r-[1.6px]  border-[#F1F2F4] pl-[3rem]  '>
-        <div className='flex gap-4 items-center'>
-        <div className="bg-[#F8F8F8] p-2 w-[3rem] h-[3rem] flex items-center justify-center rounded-full">
+            <h1 className="para text-[14px] font-medium">
+              Available <br />
+              beds
+            </h1>
+          </div>
+          <div className="text-[2rem] para font-semibold mt-[1.2rem]  mb-[-.8rem]">
+            32
+          </div>
+        </div>
+        <div className="w-full h-full border-r-[1.6px]  border-[#F1F2F4] pl-[3rem]  ">
+          <div className="flex gap-4 items-center">
+            <div className="bg-[#F8F8F8] p-2 w-[3rem] h-[3rem] flex items-center justify-center rounded-full">
+              <FaTimes className="w-3" />
+            </div>
+            <h1 className="para text-[14px] font-medium">
+              Occupied <br />
+              beds
+            </h1>
+          </div>
+          <div className="text-[2rem] para font-semibold mt-[1.2rem]  mb-[-.8rem]">
+            07
+          </div>
+        </div>
+        <div className="w-full h-full border-r-[1.6px]  border-[#F1F2F4] pl-[3rem]  ">
+          <div className="flex gap-4 items-center">
+            <div className="bg-[#F8F8F8] p-2 w-[3rem] h-[3rem] flex items-center justify-center rounded-full">
               <LucideClipboardList className="w-3" />
             </div>
-         <h1 className="para text-[14px] font-medium">Occupied <br/>beds</h1>
-           
-         </div> 
-         <div className="text-[2rem] para font-semibold mt-[1.2rem]  mb-[-.8rem]">07</div>
-      </div>
-      <div className='w-full h-full border-r-[1.6px]  border-[#F1F2F4] pl-[3rem]  '>
-        <div className='flex gap-4 items-center'>
-        <div className="bg-[#F8F8F8] p-2 w-[3rem] h-[3rem] flex items-center justify-center rounded-full">
-              <LucideClipboardList className="w-3" />
-            </div>
-         <h1 className="para text-[14px] font-medium">Transfers <br/>Work list</h1>
-           
-         </div> 
-         <div className="text-[2rem] para font-semibold mt-[1.2rem]  mb-[-.8rem]">07</div>
-      </div>
-      <div className='w-full  h-full pl-[3rem] '>
-        <div className='flex gap-4 items-center'>
-        <div className="bg-[#F8F8F8] p-2 w-[3rem] h-[3rem] flex items-center justify-center rounded-full">
+            <h1 className="para text-[14px] font-medium">
+              Transfers <br />
+              Work list
+            </h1>
+          </div>
+          <div className="text-[2rem] para font-semibold mt-[1.2rem]  mb-[-.8rem]">
+            07
+          </div>
+        </div>
+        <div className="w-full  h-full pl-[3rem] ">
+          <div className="flex gap-4 items-center">
+            <div className="bg-[#F8F8F8] p-2 w-[3rem] h-[3rem] flex items-center justify-center rounded-full">
               <IoHappyOutline className="w-3" />
             </div>
-         <h1 className="para text-[14px] font-medium">Discharge <br/>List</h1>
-           
-         </div> 
-         <div className="text-[2rem] para font-semibold mt-[1.2rem]  mb-[-.8rem]">32</div>
+            <h1 className="para text-[14px] font-medium">
+              Discharge <br />
+              List
+            </h1>
+          </div>
+          <div className="text-[2rem] para font-semibold mt-[1.2rem]  mb-[-.8rem]">
+            32
+          </div>
+        </div>
       </div>
-     </div>
-     
     </div>
-  )
-}
+  );
+};
 
-export default DataNumberTwo
+export default DataNumberTwo;
--- src/components/Tables/Navbar.jsx
+++ src/components/Tables/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { IoIosArrowBack } from 'react-icons/io';
+import { PiLessThanBold, PiLessThanLight } from 'react-icons/pi';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex justify-between px-[2rem] py-[1rem] relative">
+      <div className=''>
+        <div className="flex gap-4 items-center">
+          <div>
+            <IoIosArrowBack className="w-5 h-5 text-[#a0aec0] " />
+          </div>
+          <div className="">
+            <h1 className="text-[1.4rem] mt-2 font-semibold para">
+              Consultations (7)
+            </h1>
+          </div>
+        </div>
+        <div className="ml-[2.3rem] mt-3 para text-[#687588] text-sm">
+          <p>Manage your consultations</p>
+        </div>
+      </div>
+      <div className='w-[60%]'>
+        <ul className="flex gap-8 items-center h-full para justify-end">
+          <li className='border-b-2 pb-1 border-[#27A376]  text-[#27A376] font-medium'><span className='px-[.7rem]'>All pending</span></li>
+          <li className='pb-1 '>Today</li>
+          <li className='pb-1'>This Week</li>
+          <li className='pb-1'>This Month</li>
+        </ul> 
+        <hr className='h-2 w-full'/>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
