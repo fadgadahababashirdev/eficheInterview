fec3e55a902d48802db8c01819a2d33a22e6cfba
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-10-25 18:08:06 +0200

    adding responsive sidebar

--- src/Dashboard/NurseDashboard.jsx
+++ src/Dashboard/NurseDashboard.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import DataNumber from './DataNumber';
 import Stats from './Stats';
 
-const NurseDashboard = () => {
+const NurseDashboard = () => { 
+  const [showSideBar, setShowSideBar] = useState(true); 
+  const handleShowSideBar = ()=>{
+    setShowSideBar(!showSideBar)
+  }
   return (
     <div className="flex">
-      <div className="w-[12%] fixed left-0 top-0 h-screen  border border-r-[0.4px] flex justify-center py-[2rem]">
-        <Sidebar />
-      </div>
+     {showSideBar &&  <div className="w-[40%] px-5  md:px-0   md:w-[12%] fixed left-0 top-0 h-screen  border border-r-[0.4px] md:flex justify-center md:py-[2rem]">
+        <Sidebar handleShowSideBar={handleShowSideBar}/>
+      </div>}
 
-      <div className="w-full">
-        <Navbar />
+      <div className="w-full hidden  md:visible">
+      <Navbar />
 
         <DataNumber/> 
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,48 +1,57 @@
 import { ClipboardList, Users2, Calendar } from 'lucide-react';
-import React from 'react';
-import { FaFile } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaFile, FaTimes } from 'react-icons/fa';
 import { GoPersonAdd } from 'react-icons/go';
 import { IoGridOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+const Sidebar = ({handleShowSideBar}) => {
+ 
   return (
-    <div>
-      {/* file and heading  div*/}
-      <div>
-        <Link to="/Nurse">
-          <div className="flex justify-between h-fit gap-2">
-            <div className="flex  items-center">
-              <FaFile className="text-[1.3rem]  text-[#27a376]" />
+    <>
+    <div className="">
+        <div className="flex items-center my-2 md:my-0  md:hidden border w-10 h-10 justify-center border-slate-200 rounded-md p-2" onClick={handleShowSideBar}>
+          {' '}
+          <FaTimes className="w-4 h-4" />
+        </div>
+        
+        <div className=''>
+          <Link to="/Nurse">
+            <div className="flex justify-between h-fit gap-2">
+              <div className="flex  items-center">
+                <FaFile className="text-[1.3rem]  text-[#27a376]" />
+              </div>
+              <div>
+                <h1 className=" text-[1.6rem] font-bold">eFiche</h1>
+              </div>
             </div>
-            <div>
-              <h1 className=" text-[1.6rem] font-bold">eFiche</h1>
+          </Link>
+          {/* menu icon */}
+          <Link to="/Nurse">
+            <div className="flex justify-center bg-[#27a376] p-5 rounded-xl my-[2rem]">
+              <IoGridOutline className="text-white text-[1rem]" />
             </div>
+          </Link>
+          {/* person */}
+          <div className="flex justify-center my-[2rem]">
+            <GoPersonAdd className="text-[#a0aec0] text-[1rem]" />
           </div>
-        </Link>
-        {/* menu icon */}
-       <Link to="/Nurse">
-       <div className="flex justify-center bg-[#27a376] p-5 rounded-xl my-[2rem]">
-          <IoGridOutline className="text-white text-[1rem]" />
-        </div></Link>
-        {/* person */}
-        <div className="flex justify-center my-[2rem]">
-          <GoPersonAdd className="text-[#a0aec0] text-[1rem]" />
-        </div>
-        {/* calender */}
-        <div className="flex justify-center my-[2rem]">
-          <Calendar className="text-[#a0aec0] text-[1rem]" />
-        </div>
-        {/*clipboard list */}
-        <div className="flex justify-center my-[2rem]">
-          <ClipboardList className="text-[#a0aec0] text-[1rem]" />
-        </div>
-        {/*  */}
-        <div className="flex justify-center my-[2rem]">
-          <Users2 className="text-[#a0aec0] text-[1rem]" />
+          {/* calender */}
+          <div className="flex justify-center my-[2rem]">
+            <Calendar className="text-[#a0aec0] text-[1rem]" />
+          </div>
+          {/*clipboard list */}
+          <div className="flex justify-center my-[2rem]">
+            <ClipboardList className="text-[#a0aec0] text-[1rem]" />
+          </div>
+          {/*  */}
+          <div className="flex justify-center my-[2rem]">
+            <Users2 className="text-[#a0aec0] text-[1rem]" />
+          </div>
         </div>
       </div>
-    </div>
+     
+    </>
   );
 };
 
