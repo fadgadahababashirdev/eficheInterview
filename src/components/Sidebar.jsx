7a101e312373a0facaf6cdf5358a8b2ea30ef46c
Fadga Dahaba Bashir Dev <138160871+fadgadahababashirdev@users.noreply.github.com>
2024-10-26 12:03:17 +0200

    commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,14 +5,17 @@ import { GoPersonAdd } from 'react-icons/go';
 import { IoGridOutline } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
- 
+const Sidebar = () => {<div className="relative inline-block">
+  <div className="bg-gray-900 text-white px-6 py-2 rounded-2xl shadow-lg">
+    <span className="font-sans text-lg">My patients</span>
+  </div>
+  {/* Triangle pointer at bottom - smaller size */}
+  <div className="absolute -bottom-2 left-3 w-2 h-2 transform rotate-45 bg-gray-900"></div>
+</div>
   return (
     <>
-    <div className="">
-       
-        
-        <div className=''>
+      <div className="">
+        <div className="">
           <Link to="/Nurse">
             <div className="flex justify-between h-fit gap-2">
               <div className="flex  items-center">
@@ -30,9 +33,12 @@ const Sidebar = () => {
             </div>
           </Link>
           {/* person */}
+        
           <div className="flex justify-center my-[2rem]">
             <GoPersonAdd className="text-[#a0aec0] text-[1rem]" />
           </div>
+           
+
           {/* calender */}
           <div className="flex justify-center my-[2rem]">
             <Calendar className="text-[#a0aec0] text-[1rem]" />
@@ -47,7 +53,6 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
-     
     </>
   );
 };
